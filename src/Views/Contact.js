55960ce8cf043b2d026f9788fda6c0f5c42774de
Klaudijus <klaudijus.k1996@gmail.com>
2020-09-24 11:42:12 +0300

    contact init

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -2,26 +2,22 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <section
-            className="w-full bg-gray-300 text-center p-8"
-        >
-
-            <h1 className="text-5xl text-center">Contact FORM</h1>
-            <div className="">
-                <form
-                    className="w-1/2"
-                >
-
-                    <input className="w-1/2 bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4  appearance-none leading-normal" type="text" placeholder="John Doe" />
-
-                    <input className="w-1/2 bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4  appearance-none leading-normal" type="email" placeholder="john.doe@example.com" />
-
-
-                    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                        Submit
-                </button>
-
-                </form>
+        <section className="bg-blue-100 p-20">
+            <h1 style={{ fontFamily: "'Righteous', cursive" }} className="text-5xl text-center">Contact</h1>
+            <div className="container flex justify-between p-10">
+                <div className="w-2/5 bg-gray-200">
+                    <ul>
+                        <li className="py-4"><span className="text-red-500 mr-auto ml-auto rounded-full w-16 h-16 leading-3 bg-gray-300 text-5xl hover:bg-gray-400 hover:text-red-700">Email</span> - ###########</li>
+                        <li className="py-4"><span className="text-red-500 mr-auto ml-auto rounded-full w-16 h-16 leading-3 bg-gray-300 text-5xl hover:bg-gray-400 hover:text-red-700">Phone</span> - ###########</li>
+                        <li className="py-4"><span className="text-red-500 mr-auto ml-auto rounded-full w-16 h-16 leading-3 bg-gray-300 text-5xl hover:bg-gray-400 hover:text-red-700">Something</span> - ###########</li>
+                    </ul>
+                </div>
+                <div className="w-2/5 bg-gray-300">
+                    <textarea style={{ resize: "none" }} className="w-full h-full hover:bg-gray-100 border" name="" id=""></textarea>
+                    <button class="float-right bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                        Send
+                    </button>
+                </div>
             </div>
         </section>
     )
