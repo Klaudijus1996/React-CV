c40e5d23e895f9677e2926a85a40ee1715a5c292
Klaudijus <klaudijus.k1996@gmail.com>
2020-09-28 19:59:16 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './output.css';
+import Page from 'react-page-loading';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Heading from './Views/Heading';
@@ -10,14 +11,21 @@ import Contact from './Views/Contact'
 function App() {
   return (
     <>
-      <Header />
-      <div className="w-full">
-        <Heading />
-        <About />
-        <Skills />
-        <Contact />
-      </div>
-      <Footer />
+      <Page
+        loader={"bar"}
+        color={"#A9A9A9"}
+        size={4}
+        duration={1}
+      >
+        <Header />
+        <div className="w-full">
+          <Heading />
+          <About />
+          <Skills />
+          <Contact />
+        </div>
+        <Footer />
+      </Page>
     </>
   );
 }
