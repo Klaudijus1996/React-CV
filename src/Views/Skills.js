a6eb99078f879c83be266ec4b681ed59fd7f896d
Klaudijus <klaudijus.k1996@gmail.com>
2020-09-30 14:42:06 +0300

    skills upd

--- src/Views/Skills.js
+++ src/Views/Skills.js
@@ -19,15 +19,17 @@ const Skills = () => {
     window.addEventListener('scroll', showSection);
     return (
         <section id="skills" className="bg-gray-100 p-5">
-            <h1 style={{ fontFamily: "'Righteous', cursive" }} className="text-5xl text-center p-5">Skills</h1>
-            <div className="container mx-auto flex flex-wrap justify-between">
+            <h1 style={{ fontFamily: "'Righteous', cursive" }} className="text-4xl md:text-5xl text-center p-5">Skills</h1>
+            <div className="container mx-auto sm:flex sm:flex-wrap sm:justify-between">
                 {
                     boxTransition.map(({ item, key, props }) =>
                         item &&
                         <animated.div
                             key={key}
                             style={props}
-                            className={showElements ? "w-1/4 transition duration-500 shadow-xl p-2 text-center m-2 my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
+                            className={showElements ?
+                                " w-2/4 sm:w-2/5 lg:w-1/4 mx-auto my-3 transition duration-500 shadow-xl p-2 text-center md:mx-2 sm:my-4 hover:bg-orange-100 bg-white border-l border-b border-r"
+                                : "hidden"}>
                             <h4 className="text-orange-500 text-5xl hover:text-orange-600 h-16 w-16 mr-auto ml-auto"><FontAwesomeIcon icon={faHtml5} /></h4>
                             <p style={{ fontFamily: "'EB Garamond', serif" }} className="text-lg p-4">
                                 <ul>
@@ -45,7 +47,7 @@ const Skills = () => {
                         <animated.div
                             key={key}
                             style={props}
-                            className={showElements ? "w-1/4 transition duration-500 shadow-xl p-2 text-center m-2 my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
+                            className={showElements ? " w-2/4 sm:w-2/5 lg:w-1/4 mx-auto my-3 transition duration-500 shadow-xl p-2 text-center md:mx-2 sm:my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
                             <h4 className="text-blue-500 text-5xl hover:text-blue-700 h-16 w-16 mr-auto ml-auto"><FontAwesomeIcon icon={faCss3} /></h4>
                             <p style={{ fontFamily: "'EB Garamond', serif" }} className="text-lg p-4">
                                 <ul>
@@ -63,7 +65,7 @@ const Skills = () => {
                         <animated.div
                             key={key}
                             style={props}
-                            className={showElements ? "w-1/4 transition duration-500 shadow-xl p-2 text-center m-2 my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
+                            className={showElements ? " w-2/4 sm:w-2/5 lg:w-1/4 mx-auto my-3 transition duration-500 shadow-xl p-2 text-center md:mx-2 sm:my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
                             <h4 className="text-yellow-400 text-5xl hover:text-yellow-500 w-16 h-16 mr-auto ml-auto"><FontAwesomeIcon icon={faJs} /></h4>
                             <p style={{ fontFamily: "'EB Garamond', serif" }} className="text-lg p-4">
                                 <ul>
@@ -81,7 +83,7 @@ const Skills = () => {
                         <animated.div
                             key={key}
                             style={props}
-                            className={showElements ? "w-1/4 transition duration-500 shadow-xl p-2 text-center m-2 my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
+                            className={showElements ? " w-2/4 sm:w-2/5 lg:w-1/4 mx-auto my-3 transition duration-500 shadow-xl p-2 text-center md:mx-2 sm:my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
                             <h4 className="text-red-500 mr-auto ml-auto rounded-full w-16 h-16 leading-3 bg-gray-300 text-5xl hover:bg-gray-400 hover:text-red-700"><FontAwesomeIcon icon={faJava} /></h4>
                             <p style={{ fontFamily: "'EB Garamond', serif" }} className="text-lg p-4">
                                 <ul>
@@ -99,7 +101,7 @@ const Skills = () => {
                         <animated.div
                             key={key}
                             style={props}
-                            className={showElements ? "w-1/4 transition duration-500 shadow-xl p-2 text-center m-2 my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
+                            className={showElements ? " w-2/4 sm:w-2/5 lg:w-1/4 mx-auto my-3 transition duration-500 shadow-xl p-2 text-center lg:mx-2 sm:my-4 hover:bg-orange-100 bg-white border-l border-b border-r" : "hidden"}>
                             <h4 className="text-purple-500 text-5xl hover:text-purple-600 h-16 w-16 mr-auto ml-auto"><FontAwesomeIcon icon={faPhp} /></h4>
                             <p style={{ fontFamily: "'EB Garamond', serif" }} className="text-lg p-4">
                                 <ul>
