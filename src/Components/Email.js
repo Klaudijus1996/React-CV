933e4b46df3787486e74c09b2c966f12aed502af
Klaudijus <klaudijus.k1996@gmail.com>
2020-09-30 22:57:29 +0300

    upd

---
+++ src/Components/Email.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import emailjs from 'emailjs-com';
+
+function Email() {
+    const [subject, setSubject] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+    const [errors, setErrors] = useState({});
+    const [successMessage, setSuccessMessage] = useState("");
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        const isValid = validateForm();
+        if (isValid) {
+            emailjs.sendForm('service_9lngbfl', 'default1', e.target, 'user_huJu7HpHM4qxPtURXCKez')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+            e.target.reset();
+        } else {
+            setSuccessMessage("");
+        }
+    }
+    function validateForm() {
+        const errors = {};
+        let isValid = true;
+
+        if (subject.trim().length > 10) {
+            errors.subjectLength = "Error: subject must contain less than 10 characters!";
+            isValid = false;
+        }
+        if (subject.trim().length < 1 || name.trim().length < 1 || message.length < 1) {
+            errors.emptyString = "Error: All fields are required!";
+            isValid = false;
+        }
+        if (subject.match(/delete/ig) || name.match(/delete/ig) || message.match(/delete/ig)) {
+            errors.deleteFalse = "ERROR: forbidden input...";
+        }
+        if (name.trim().length > 12) {
+            errors.nameLength = "Error: name field must be less than 12 characters!";
+            isValid = false;
+        }
+        if (name.match(/[+=_\-)(*&^%$#@!/?.>,<'";:[{\]}\\|0-9]/ig)) {
+            errors.containsSymbols = "Error: name must contain only alphabetic characters!";
+            isValid = false;
+        }
+        if (isValid) {
+            setSuccessMessage("Your message has been sent!");
+        }
+        setErrors(errors);
+        return isValid;
+    }
+    return (
+        <>
+            {
+                Object.keys(errors).map((key) => {
+                    return <div key={key} className="w-full text-red-700 bg-red-300 mb-2 px-4 py-2 cursor-default">{errors[key]}</div>
+                })
+            }
+            {
+                successMessage !== "" ? <div className="w-full text-green-700 bg-green-300 mb-2 px-4 py-2 cursor-default">{successMessage}</div> : null
+            }
+
+            <form onSubmit={submitForm} className="w-full h-full">
+                <div className="w-full border mb-2">
+                    <input
+                        className="px-4 py-2 w-full focus:outline-none"
+                        value={subject}
+                        onChange={(e) => { setSubject(e.target.value) }}
+                        autoComplete="off"
+                        type="text"
+                        placeholder="*Subject"
+                        name="subject" />
+                </div>
+                <div className="w-full border mb-2">
+                    <input
+                        className="px-4 py-2 w-full focus:outline-none"
+                        value={name}
+                        onChange={(e) => { setName(e.target.value) }}
+                        autoComplete="off"
+                        type="text"
+                        placeholder="*Your name"
+                        name="name" />
+                </div>
+                <div className="w-full">
+                    <textarea
+                        style={{ resize: "none" }}
+                        value={message}
+                        onChange={(e) => { setMessage(e.target.value) }}
+                        className="px-4 py-2 w-full h-56 border hover:bg-gray-100 border focus:outline-none"
+                        placeholder="Send me an Email!"
+                        name="message"
+                        id="" />
+                </div>
+                <div className="w-full text-center">
+                    <input
+                        type="submit"
+                        className="my-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                        value="Send!" />
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default Email
--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight, faEnvelope, faMapMarkerAlt, faMobileAlt } from '@fortawesome/free-solid-svg-icons';
-
+import Email from '../Components/Email';
 const Contact = () => {
     const [showElements, setShowElements] = useState(false);
     const showSection = () => {
@@ -16,30 +16,25 @@ const Contact = () => {
             <div className="container mx-auto lg:flex justify-between p-10">
                 <div className={showElements ? "w-4/5 lg:w-2/5 bg-white mx-auto lg:mx-0" : "hidden"}>
                     <ul className="w-auto sm:w-10/12 md:w-8/12 lg:w-full xl:w-4/5 mr-auto ml-auto">
-                        <li className={showElements ? "py-4 flex items-center" : "hidden"}>
+                        <li className={showElements ? "py-4 sm:flex items-center" : "hidden"}>
                             <h4 className="inline-block text-center text-red-400 rounded-full w-20 h-20 leading-2 bg-white text-5xl hover:bg-gray-100 hover:text-red-500">
                                 <FontAwesomeIcon icon={faEnvelope} />
-                            </h4><p className="leading-3 text-lg inline-block">&nbsp; <FontAwesomeIcon icon={faArrowRight} /> klaudijus.k1996@gmail.com</p>
+                            </h4><p className="leading-3 break-all sm:break-normal text-lg inline-block">&nbsp; <FontAwesomeIcon className="hidden sm:contents" icon={faArrowRight} /> klaudijus.k1996@gmail.com</p>
                         </li>
-                        <li className={showElements ? "py-4 flex items-center" : "hidden"}>
+                        <li className={showElements ? "py-4 sm:flex items-center" : "hidden"}>
                             <h4 className="inline-block text-center text-gray-700 rounded-full w-20 h-20 leading-2 bg-white text-5xl hover:bg-gray-100 hover:text-gray-800">
                                 <FontAwesomeIcon icon={faMobileAlt} />
-                            </h4><p className="leading-3 text-lg inline-block">&nbsp; <FontAwesomeIcon icon={faArrowRight} /> +37067035988</p>
+                            </h4><p className="leading-3 break-all sm:break-normal text-lg inline-block">&nbsp; <FontAwesomeIcon className="hidden sm:contents" icon={faArrowRight} /> +37067035988</p>
                         </li>
-                        <li className={showElements ? "py-4 flex items-center" : "hidden"}>
+                        <li className={showElements ? "py-4 sm:flex items-center" : "hidden"}>
                             <h4 className="inline-block text-center rounded-full w-20 h-20 leading-2 text-red-300 text-5xl bg-white hover:bg-gray-100 hover:text-red-500">
                                 <FontAwesomeIcon icon={faMapMarkerAlt} />
-                            </h4><p className="leading-3 text-lg inline-block">&nbsp; <FontAwesomeIcon icon={faArrowRight} /> Partizanų g, Kaunas, Lithuania</p>
+                            </h4><p className="leading-3 break-all sm:break-normal text-lg inline-block">&nbsp; <FontAwesomeIcon className="hidden sm:contents" icon={faArrowRight} /> Partizanų g, Kaunas, Lithuania</p>
                         </li>
                     </ul>
                 </div>
-                <div className={showElements ? "w-4/5 lg:w-2/5 h-64 lg:h-auto shadow-lg mx-auto lg:mx-0 mt-10 lg:mt-0" : "hidden"}>
-                    <textarea style={{ resize: "none" }} className="w-full h-64 lg:h-full hover:bg-gray-100 border focus:outline-none p-2" placeholder="Send me an Email!" name="" id=""></textarea>
-                    <div className="w-full text-center">
-                        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                            Send
-                        </button>
-                    </div>
+                <div className={showElements ? "w-4/5 lg:w-2/5 h-auto lg:h-auto shadow-sm mx-auto lg:mx-0 mt-10 lg:mt-0" : "hidden"}>
+                    <Email />
                 </div>
             </div>
         </section>
