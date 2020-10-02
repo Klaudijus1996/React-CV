62cefbb6488018f0fe10e67529500598b60020d1
Klaudijus <klaudijus.k1996@gmail.com>
2020-10-02 15:53:50 +0300

    upd

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -4,7 +4,7 @@ import { faArrowDown, faArrowRight, faEnvelope, faMapMarkerAlt, faMobileAlt } fr
 import Email from '../Components/Email';
 const Contact = () => {
     const [showElements, setShowElements] = useState(false);
-    const [switchElement, setSwitchElement] = useState(false);
+    const [switchElement, setSwitchElement] = useState(true);
     const switchElements = () => {
         return window.innerWidth > 420 ? setSwitchElement(true) : setSwitchElement(false);
     }
@@ -19,34 +19,46 @@ const Contact = () => {
         <section id="contact" className="bg-blue-100 p-5 pb-32">
             <h1 style={{ fontFamily: "'Righteous', cursive" }} className="text-5xl text-center p-5">Contact</h1>
             <div className="container mx-auto lg:flex justify-between p-10">
-                <div className={showElements ? "w-4/5 lg:w-2/5 bg-white mx-auto lg:mx-0" : "hidden"}>
+                <div className={showElements ? "w-4/5 lg:w-2/5 mx-auto lg:mx-0" : "hidden"}>
                     <ul
                         className="
                     w-auto sm:w-10/12 md:w-8/12 lg:w-full xl:w-4/5 
                     text-sm sm:text-base md:text-lg
                     mr-auto ml-auto">
                         <li className={showElements ? "py-4 flex" : "hidden"}>
-                            <div className={switchElement ? "text-sm sm:text-base md:text-lg flex mx-auto items-center text-center" :
+                            <div className={window.innerWidth > 420 ? "text-sm sm:text-base md:text-lg flex mx-auto items-center text-center" :
                                 "text-sm sm:text-base md:text-lg mx-auto items-center text-center"}>
-                                <p className=""><FontAwesomeIcon className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl" icon={faEnvelope} /> </p>
-                                <p>{switchElement ? <>&nbsp;<FontAwesomeIcon icon={faArrowRight} /></> : <FontAwesomeIcon icon={faArrowDown} />}</p>
-                                <p>&nbsp;klaudijus.k1996@gmail.com</p>
+                                <p className="py-2 px-2 bg-white text-red-500 hover:bg-gray-100 hover:text-red-700 mx-auto rounded-full" style={{ width: "fit-content" }}><FontAwesomeIcon className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl" icon={faEnvelope} /> </p>
+                                {
+                                    window.innerWidth > 420 ?
+                                        <p className="py-1 px-2">&nbsp;<FontAwesomeIcon icon={faArrowRight} /></p> :
+                                        <p className="py-1 px-2"><FontAwesomeIcon icon={faArrowDown} /></p>
+                                }
+                                <p className="py-1 px-2">&nbsp;klaudijus.k1996@gmail</p>
                             </div>
                         </li>
                         <li className={showElements ? "py-4 flex" : "hidden"}>
-                            <div className={switchElement ? "text-sm sm:text-base md:text-lg flex mx-auto items-center text-center" :
+                            <div className={window.innerWidth > 420 ? "text-sm sm:text-base md:text-lg flex mx-auto items-center text-center" :
                                 "text-sm sm:text-base md:text-lg mx-auto items-center text-center"}>
-                                <p className=""><FontAwesomeIcon className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl" icon={faMobileAlt} /> </p>
-                                <p>{switchElement ? <>&nbsp;<FontAwesomeIcon icon={faArrowRight} /></> : <FontAwesomeIcon icon={faArrowDown} />}</p>
-                                <p>&nbsp;+37067035988</p>
+                                <p className="py-2 px-3 bg-white text-blue-700 hover:bg-gray-100 hover:text-blue-900 mx-auto rounded-full" style={{ width: "fit-content" }}><FontAwesomeIcon className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl" icon={faMobileAlt} /> </p>
+                                {
+                                    window.innerWidth > 420 ?
+                                        <p className="py-1 px-2">&nbsp;<FontAwesomeIcon icon={faArrowRight} /></p> :
+                                        <p className="py-1 px-2"><FontAwesomeIcon icon={faArrowDown} /></p>
+                                }
+                                <p className="py-1 px-2">&nbsp;+37067035988</p>
                             </div>
                         </li>
                         <li className={showElements ? "py-4 flex" : "hidden"}>
-                            <div className={switchElement ? "text-sm sm:text-base md:text-lg flex mx-auto items-center text-center" :
+                            <div className={window.innerWidth > 420 ? "text-sm sm:text-base md:text-lg flex mx-auto items-center text-center" :
                                 "text-sm sm:text-base md:text-lg mx-auto items-center text-center"}>
-                                <p className=""><FontAwesomeIcon className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl" icon={faMapMarkerAlt} /> </p>
-                                <p>{switchElement ? <>&nbsp;<FontAwesomeIcon icon={faArrowRight} /></> : <FontAwesomeIcon icon={faArrowDown} />}</p>
-                                <p>&nbsp;Partizanų g, Kaunas, Lithuania</p>
+                                <p className="py-2 px-3 bg-green-300 text-red-500 hover:bg-green-400 hover:text-red-700 mx-auto rounded-full" style={{ width: "fit-content" }}><FontAwesomeIcon className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl" icon={faMapMarkerAlt} /> </p>
+                                {
+                                    window.innerWidth > 420 ?
+                                        <p className="py-1 px-2">&nbsp;<FontAwesomeIcon icon={faArrowRight} /></p> :
+                                        <p className="py-1 px-2"><FontAwesomeIcon icon={faArrowDown} /></p>
+                                }
+                                <p className="py-1 px-2">&nbsp;Partizanų g, Kaunas, Lithuania</p>
                             </div>
                         </li>
                     </ul>
